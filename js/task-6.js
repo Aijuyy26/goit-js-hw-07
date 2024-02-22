df16d7839250198fa802f363a8a0710b07ef6e8b
Aijuyy26 <alexandralevchuk6@gmail.com>
2024-02-22 16:29:46 +0200

    fgj

--- js/task-6.js
+++ js/task-6.js
@@ -1,9 +1,11 @@
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
 }
 
 const controls = document.getElementById('controls');
-const input = controls.querySelector('#input');
+const input = controls.querySelector('input[type="number"]');
 const createButton = controls.querySelector('button[data-action="create"]');
 const destroyButton = controls.querySelector('button[data-action="destroy"]');
 const boxesContainer = document.getElementById('boxes');
